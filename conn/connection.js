fb482354ebde8f5b80dbc64999bc06c42372f8c1
nokuthula431 <santranokuthula@gmail.com>
2020-01-28 14:39:52 +0200

    rasta

---
+++ conn/connection.js
@@ -0,0 +1,12 @@
+const mongosose = requere('mongoose');
+
+//connect to mangodb
+mongosose.connect('mongodb://localhost/testaroo');
+
+mongosose.connection.once('open'.funtion(),
+{
+    
+})
+var MongoClient = require('mongodb').MongoClient;
+//Create a database named "mydb":
+
