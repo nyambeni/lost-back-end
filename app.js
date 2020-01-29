1973925c52f49aa957faa07836545ad8f7e06fd4
nyambeni <53510131+nyambeni@users.noreply.github.com>
2020-01-29 12:00:57 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+var cors = require('cors')
+const mysqlConn= require('./conn/con');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+// api routes
+
+ app.use('/', require('./routes/registration'));
+ app.use('/', require('./routes/login'));
+ app.use('/', require('./routes/admin'));
+ app.use('/', require('./routes/index'));
+ 
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
